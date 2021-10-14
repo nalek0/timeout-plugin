902ccadac860fb4a7ed5f22f2f3f2e0ef79c8be5
nalek0 <belash.aleksandr777@gmail.com>
2021-10-14 14:21:07 +0300

    First version, only block-current and ama-free commands

---
+++ background.js
@@ -0,0 +1,81 @@
+class TimeoutWorkerClass {
+	addSite(urlPath, time) {
+		let urlObject = new URL(urlPath)
+		let currentTime = Date.now()
+		let blockUntil = currentTime + time
+		let newNote = {}
+		newNote[urlObject.origin] = blockUntil
+		chrome.storage.local.set(newNote, () => {
+			console.log(`Set item: ${urlObject.origin} = ${blockUntil}`)
+		})
+	}
+
+	removeAllSites() {
+		chrome.storage.local.clear(() => {
+			console.log("Removed all sites")
+		})
+	}
+
+	removeSite(urlPath) {
+		let urlObject = URL(urlPath)
+		chrome.storage.local.remove(urlObject.origin, () => {
+			console.log(`Removed item: ${urlObject.origin}`)
+		})
+	}
+
+	blockTab(tab) {
+		console.log("Blocking tab:", tab)
+		chrome.tabs.remove(tab.id)
+	}
+
+	checkTab(tab) {
+		let urlObject = new URL(tab.url)
+		let currentTime = Date.now()
+		chrome.storage.local.get(urlObject.origin, results => {
+			let blockUntil = results[urlObject.origin]
+			if (blockUntil != null) {
+				blockUntil = parseInt(blockUntil)
+				if (!isNaN(blockUntil) && currentTime < blockUntil) 
+					this.blockTab(tab)
+			}
+		})
+	}
+}
+
+var TimeoutWorker = new TimeoutWorkerClass()
+
+chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => checkTab(tabId, changeInfo, tab))
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
+}
+
+async function checkTab(tabId, changeInfo, tab) {
+	if (tab.status == "complete")
+		TimeoutWorker.checkTab(tab)
+}
+
+chrome.contextMenus.create({
+	id: "block-current",
+	title: "Block current site for 30 minutes",
+	contexts: ["all"]
+})
+
+chrome.contextMenus.create({
+	id: "ama-free",
+	title: "Unblock all sites",
+	contexts: ["all"]
+})
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+	if (info.menuItemId == "block-current") {
+		let currentTab = await getCurrentTab()
+		TimeoutWorker.addSite(currentTab.url, 30 * 60 * 1000)
+		TimeoutWorker.blockTab(currentTab)
+	}
+	else if (info.menuItemId == "ama-free") {
+		TimeoutWorker.removeAllSites()
+	}
+})
